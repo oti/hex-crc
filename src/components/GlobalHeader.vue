72ae1acaf9c1a9090128c515719c4f0bee4f5956
oti <otiext@gmail.com>
2019-10-26 15:08:40 +0900

    GlobalHeaderコンポーネントを追加した

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="GlobalHeader">
+    <h1 class="__heading">HEX Contrast Ratio Calcurator</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class GlobalHeader extends Vue {}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/styles/mixins'
+@import '../assets/styles/placeholders'
+
+.GlobalHeader
+  display: flex
+  justify-content: center
+  align-items: center
+  padding: 1.25rem
+  color: var(--colorBg)
+  background-color: var(--colorText)
+
+  .__heading
+    margin: 0
+    font-size: 1.6rem
+    line-height: 1.2
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div class="Home">
+    <GlobalHeader />
+  </div>
 </template>
 
 <script lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
-@Component
+@Component({
+  components: {
+    GlobalHeader
+  }
+})
 export default class Home extends Vue {}
 </script>
