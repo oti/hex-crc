28beb96416db06618cd504c4212dffe490489fcb
oti <otiext@gmail.com>
2019-11-05 16:19:05 +0900

    UiストアのdarkをトグルするThemeTogglerコンポーネントを追加した

---
+++ src/components/ThemeToggler.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="ThemeToggler">
+    <input
+      id="ThemeTogglerInput"
+      v-model="ui.dark"
+      class="__input"
+      type="checkbox"
+      name="darkMode"
+      @change="handleToggleDarkMode"
+    />
+    <label class="__label" for="ThemeTogglerInput">{{ toggleThemeText }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { UiActionDispatchers, UiViewModel } from '@/store/modules/ui/models'
+import { Component, Vue } from 'vue-property-decorator'
+import { Action, Getter } from 'vuex-class'
+
+@Component
+export default class ThemeToggler extends Vue {
+  // viewModel を引き当てる
+  @Getter('ui/viewModel') ui!: UiViewModel
+
+  /**
+   * アクションを引き当てる
+   */
+  @Action('ui/toggleDarkMode')
+  toggleDarkMode!: UiActionDispatchers['toggleDarkMode']
+
+  /**
+   * @get テーマトグルのテキストを返す
+   */
+  get toggleThemeText() {
+    return this.ui.dark ? 'ライトモードにする' : 'ダークモードにする'
+  }
+
+  /**
+   * @listens input.change
+   */
+  handleToggleDarkMode() {
+    this.toggleDarkMode(this.ui.dark)
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/styles/configs'
+
+.ThemeToggler
+  float: right
+  display: flex
+  align-items: center
+  font-size: 0.5rem
+
+  .__input
+    margin: 0
+    padding: 0
+    line-height: 0
+    vartical-align: top
+
+  .__label
+    margin-left: 5px
+</style>
