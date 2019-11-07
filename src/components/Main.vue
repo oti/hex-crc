8d2e56c091c67c2396e9a6809e29ba1f2d1b2436
oti <otiext@gmail.com>
2019-11-08 00:03:09 +0900

    <main>をMainコンポーネントに切り出した

---
+++ src/components/Main.vue
@@ -0,0 +1,52 @@
+<template>
+  <main class="Main" role="main">
+    <SectionIntro class="__intro" />
+
+    <SectionTextArea v-if="ui.showsTextArea" class="__textarea" />
+
+    <SectionColorSet class="__set" />
+  </main>
+</template>
+
+<script lang="ts">
+import SectionColorSet from '@/components/SectionColorSet.vue'
+import SectionIntro from '@/components/SectionIntro.vue'
+import SectionTextArea from '@/components/SectionTextArea.vue'
+import { UiViewModel } from '@/store/modules/ui/models'
+import { Component, Vue } from 'vue-property-decorator'
+import { Getter } from 'vuex-class'
+
+@Component({
+  components: {
+    SectionColorSet,
+    SectionIntro,
+    SectionTextArea
+  }
+})
+export default class Main extends Vue {
+  /**
+   * viewModel を引き当てる
+   */
+  @Getter('ui/viewModel') ui!: UiViewModel
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/styles/configs'
+.Main
+  margin: 2rem auto
+  padding-right: calc(var(--spaceGap) * 2)
+  padding-left: calc(var(--spaceGap) * 2)
+  max-width: calc(var(--spaceGap) * 4 + 600px)
+  font-size: 0.7rem
+
+  .__intro
+    margin-top: calc(var(--spaceGap) * 4)
+
+  .__textarea
+    margin-top: calc(var(--spaceGap) * 1.5)
+
+  .__set
+    margin-top: calc(var(--spaceGap) * 4)
+    font-size: 1rem
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="Home">
+  <div>
     <Header />
-    <main class="__main" role="main">
-      <SectionIntro class="__intro" />
 
-      <SectionTextArea v-if="ui.showsTextArea" class="__textarea" />
+    <Main />
 
-      <SectionColorSet class="__set" />
-    </main>
     <Footer />
   </div>
 </template>
@@ -15,47 +11,17 @@
 <script lang="ts">
 import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
-import SectionColorSet from '@/components/SectionColorSet.vue'
-import SectionIntro from '@/components/SectionIntro.vue'
-import SectionTextArea from '@/components/SectionTextArea.vue'
-import { UiViewModel } from '@/store/modules/ui/models'
-import { Component, Prop, Vue } from 'vue-property-decorator'
-import { Getter } from 'vuex-class'
+import Main from '@/components/Main.vue'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
   components: {
     Footer,
     Header,
-    SectionColorSet,
-    SectionIntro,
-    SectionTextArea
+    Main
   }
 })
-export default class Home extends Vue {
-  /**
-   * viewModel を引き当てる
-   */
-  @Getter('ui/viewModel') ui!: UiViewModel
-}
+export default class Home extends Vue {}
 </script>
 
-<style lang="sass" scoped>
-@import '../assets/styles/configs'
-.Home
-  .__main
-    margin: 2rem auto
-    padding-right: calc(var(--spaceGap) * 2)
-    padding-left: calc(var(--spaceGap) * 2)
-    max-width: calc(var(--spaceGap) * 4 + 600px)
-    font-size: 0.7rem
-
-  .__intro
-    margin-top: calc(var(--spaceGap) * 4)
-
-  .__textarea
-    margin-top: calc(var(--spaceGap) * 1.5)
-
-  .__set
-    margin-top: calc(var(--spaceGap) * 4)
-    font-size: 1rem
-</style>
+<style lang="sass" scoped></style>
