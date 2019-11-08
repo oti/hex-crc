ee6014fb15b7b5b88404fada8018d31e331a86dd
oti <otiext@gmail.com>
2019-11-08 12:36:42 +0900

    ColorItemLevelコンポーネントをリファクタリングした

--- src/components/ColorItemLevel.vue
+++ src/components/ColorItemLevel.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="ColorItemLevel">
-    <p :class="['__label', { VisuallyHidden: !showsLabel }]">WCAG2.0達成</p>
+    <p :class="['__label', { VisuallyHidden: !showsLabel }]">WCAG2.1達成</p>
     <p
       class="__text"
       :style="`color: ${value.front}; background-color: ${value.back}`"
     >
-      {{ level }}
+      {{ result }}
     </p>
   </div>
 </template>
 
 <script lang="ts">
 import { ColorItem } from '@/models/ColorItem'
+import { Result } from '@/models/Result'
+import { UiViewModel } from '@/store/modules/ui/models'
 import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Getter } from 'vuex-class'
 
 @Component
 export default class ColorItemLevel extends Vue {
+  /**
+   * viewModel を引き当てる
+   */
+  @Getter('ui/viewModel') ui!: UiViewModel
+
   /**
    * 入力プロパティを定義する
    */
@@ -24,17 +32,25 @@ export default class ColorItemLevel extends Vue {
   @Prop({ required: true }) value!: ColorItem
 
   /**
-   * @get - WCAG2.0において達成しているレベルを返す
+   * @get - WCAG2.1において達成している結果を返す
    */
-  get level() {
-    const { ratio } = this
-    if (ratio >= 7) {
-      return 'AAA'
-    } else if (ratio >= 4.5) {
-      return 'AA'
-    } else {
-      return 'Fail'
+  get result() {
+    const { ratio, ui } = this
+    const AAA: number = ui.largeText ? 4.5 : 7
+    const AA: number = ui.largeText ? 3 : 4.5
+    let result: Result = 'Fail'
+
+    if (ratio >= 3) {
+      result = 'Not Bad'
     }
+    if (ratio >= AA) {
+      result = 'AA'
+    }
+    if (ratio >= AAA) {
+      result = 'AAA'
+    }
+
+    return result
   }
 }
 </script>
