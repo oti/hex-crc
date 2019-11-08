3a3c605a25f525bd30572cf8aef36c0339936a26
oti <otiext@gmail.com>
2019-11-08 12:27:14 +0900

    イントロにテキストを追加した

--- src/components/SectionIntro.vue
+++ src/components/SectionIntro.vue
@@ -3,14 +3,44 @@
     <ThemeToggler />
     <h2 class="SectionHeading">なにこれ？</h2>
     <p>
-      HEXの2色（前景色と背景色）のカラーセットからコントラスト比を計算し、WCAG2.0が定める基準を達成しているかを判定します。
+      HEXの2色（前景色と背景色）のカラーセットからコントラスト比を計算し、WCAG2.1が定める基準を達成しているかを検証します。
     </p>
     <ul>
       <li>HEX以外を入力すると正しく判定できません</li>
       <li>複数のカラーセットを比較できます</li>
-      <li>テキストエリアに連想配列を入力してカラーセットに変換できます</li>
-      <li>逆に、調整したカラーセットを連想配列に変換できます</li>
+      <li>
+        テキストエリアにオブジェクトの配列を入力してカラーセットに変換できます
+      </li>
+      <li>逆に、調整したカラーセットをオブジェクトの配列に変換できます</li>
     </ul>
+    <p>達成レベルのAAとAAAについては下記のリンクを参考にしています。</p>
+    <ul>
+      <li>
+        <a
+          href="https://waic.jp/docs/WCAG21/#contrast-minimum"
+          target="_blank"
+          rel="noopener"
+          >WCAG2.1 達成基準 1.4.3 コントラスト (最低）</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://waic.jp/docs/WCAG21/#contrast-enhanced"
+          target="_blank"
+          rel="noopener"
+          >WCAG2.1 達成基準 1.4.6 コントラスト (高度）</a
+        >
+      </li>
+    </ul>
+    <p>
+      WCAG2.1にはコントラスト比についてレベルA（シングルA）の達成基準がありません。本検証ツールではコントラスト比が
+      4.5 未満で 3.0 以上は「Not Bad」とし、3.0 未満は「Fail」としています。
+    </p>
+    <p>
+      <small
+        >*「大きなテキスト」とは日本語の場合、22pt以上かもしくは太字で18pt以上のテキストの場合を差します。</small
+      >
+    </p>
     <button class="Button -invert" type="button" @click="toggleTextArea">
       <i class="material-icons" aria-hidden="true">flip</i>
       <span v-if="ui.showsTextArea">テキストエリアを隠す</span>
