43b02cef514afd6c4e6effcf50acf6f561e84ae6
oti <otiext@gmail.com>
2020-01-20 18:01:38 +0900

    UI ストアからテキストエリア表示切り替えに関する実装を削除した

--- src/store/modules/ui/models.ts
+++ src/store/modules/ui/models.ts
@@ -3,7 +3,6 @@ import {
   DECREMENT_GLOBAL_LOADING_QUEUE,
   INCREMENT_GLOBAL_LOADING_QUEUE,
   TOGGLE_DARK_MODE,
-  TOGGLE_TEXT_AREA,
   TOGGLE_TEXT_SIZE
 } from '@/store/modules/ui/mutation-types'
 
@@ -11,7 +10,6 @@ export interface UiState {
   darkMode: boolean
   globalLadingQueue: number
   largeText: boolean
-  showsTextArea: boolean
 }
 
 export interface UiViewModel extends UiState {
@@ -27,7 +25,6 @@ export interface UiMutationPayloads {
   [DECREMENT_GLOBAL_LOADING_QUEUE]: number | undefined
   [INCREMENT_GLOBAL_LOADING_QUEUE]: number | undefined
   [TOGGLE_DARK_MODE]: boolean
-  [TOGGLE_TEXT_AREA]: boolean
   [TOGGLE_TEXT_SIZE]: boolean
 }
 
@@ -35,7 +32,6 @@ export interface UiActionPayloads {
   decrementGlobalLoadingQueue: UiMutationPayloads[typeof DECREMENT_GLOBAL_LOADING_QUEUE]
   incrementGlobalLoadingQueue: UiMutationPayloads[typeof INCREMENT_GLOBAL_LOADING_QUEUE]
   toggleDarkMode: UiMutationPayloads[typeof TOGGLE_DARK_MODE]
-  toggleTextArea: UiMutationPayloads[typeof TOGGLE_TEXT_AREA]
   toggleTextSize: UiMutationPayloads[typeof TOGGLE_TEXT_SIZE]
 }
 
