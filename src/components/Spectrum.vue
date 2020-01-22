9e5cde7fb65a047d23e9accb3cf8c94e789dcb99
oti <otiext@gmail.com>
2020-01-22 10:20:56 +0900

    OGP画像でふんだんに使ったスペクトルグラデーションをサイト側にもアクセントでつけた

---
+++ src/components/Spectrum.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Spectrum" />
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Spectrum extends Vue {}
+</script>
+
+<style lang="sass">
+.Spectrum
+  height: 2px
+  background-image: linear-gradient(to left, red, yellow, lime, cyan, blue, magenta)
+</style>
