4a1ba0d7b5cc07473da26a0bdeca76affc4751a3
oti <otiext@gmail.com>
2020-01-22 13:20:48 +0900

    プリセットのカラーセットを変更した - デフォルトがあまりに素人くさかった

--- src/configs/PresetColorItems.ts
+++ src/configs/PresetColorItems.ts
@@ -1,10 +1,9 @@
 import { ColorItem } from '@/models/ColorItem'
 
 export const PresetColorItems: ColorItem[] = [
-  { front: '#ff0000', back: '#ffffff' },
-  { front: '#00ff00', back: '#ffffff' },
-  { front: '#0000ff', back: '#ffffff' },
-  { front: '#ff0000', back: '#000000' },
-  { front: '#00ff00', back: '#000000' },
-  { front: '#0000ff', back: '#000000' }
+  { front: '#e20000', back: '#f5f4f5' },
+  { front: '#ffc100', back: '#f5f4f5' },
+  { front: '#00960a', back: '#f5f4f5' },
+  { front: '#008af7', back: '#f5f4f5' },
+  { front: '#8700b9', back: '#f5f4f5' }
 ]
