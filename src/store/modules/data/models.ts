ab3b4500696541ef41f360b842207d2352f498c8
Yoshiaki Ito <ito@maboroshi.biz>
2020-05-07 23:30:41 +0900

    Data ストアモデルに updateColor を追加した

--- src/store/modules/data/models.ts
+++ src/store/modules/data/models.ts
@@ -6,12 +6,19 @@ import {
   REMOVE_COLOR_ITEM,
   CONVERT_COLOR_ITEMS_TO_STRING,
   CONVERT_STRING_TO_COLOR_ITEMS,
+  UPDATE_COLOR,
   UPDATE_TEXT_AREA_VALUE
 } from '@/store/modules/data/mutation-types'
 
 export interface DataState {
   colorItems: ColorItem[]
   string: string
+}
+
+export interface UpdateColor {
+  color: string
+  index: number
+  target: 'back' | 'front'
 }
 
 export interface DataViewModel extends DataState {}
@@ -26,6 +33,7 @@ export interface DataMutationPayloads {
   [REMOVE_COLOR_ITEM]: number
   [CONVERT_COLOR_ITEMS_TO_STRING]: string
   [CONVERT_STRING_TO_COLOR_ITEMS]: ColorItem[]
+  [UPDATE_COLOR]: UpdateColor
   [UPDATE_TEXT_AREA_VALUE]: string
 }
 
@@ -35,6 +43,7 @@ export interface DataActionPayloads {
   removeColorItem: DataMutationPayloads[typeof REMOVE_COLOR_ITEM]
   convertStringToColorItems: DataMutationPayloads[typeof CONVERT_COLOR_ITEMS_TO_STRING]
   convertColorItemsToString: DataMutationPayloads[typeof CONVERT_STRING_TO_COLOR_ITEMS]
+  updateColor: DataMutationPayloads[typeof UPDATE_COLOR]
   updateTextAreaValue: DataMutationPayloads[typeof UPDATE_TEXT_AREA_VALUE]
 }
 
