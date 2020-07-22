8809f83c7798456f3456b8192729dd5aebcf3f70
oti <otiext@gmail.com>
2020-07-22 10:04:25 +0900

    feat(configs): 初期表示の色セットを DecomojiColors に変更した

--- src/configs/PresetColorItems.ts
+++ src/configs/PresetColorItems.ts
@@ -1,9 +1,8 @@
 import { ColorItem } from "@/models/ColorItem";
 
-export const PresetColorItems: ColorItem[] = [
-  { front: "#e20000", back: "#f5f4f5" },
-  { front: "#ffc100", back: "#f5f4f5" },
-  { front: "#00960a", back: "#f5f4f5" },
-  { front: "#008af7", back: "#f5f4f5" },
-  { front: "#8700b9", back: "#f5f4f5" },
-];
+import { DecomojiColors } from "@decomoji/colors/src/DecomojiColors";
+
+export const PresetColorItems: ColorItem[] = DecomojiColors.map((v) => ({
+  front: `#${v.hex}`,
+  back: "#f6f6f6",
+}));
