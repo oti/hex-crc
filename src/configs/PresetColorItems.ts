2f009ffb8f5d8d06a9f2067d19894a99a5b6428f
oti <otiext@gmail.com>
2023-06-15 19:17:56 +0900

    remove: old envs

--- src/configs/PresetColorItems.ts
+++
@@ -1,8 +0,0 @@
-import { ColorItem } from "@/models/ColorItem";
-
-import { DecomojiColors } from "@decomoji/colors/src/DecomojiColors";
-
-export const PresetColorItems: ColorItem[] = DecomojiColors.map((v) => ({
-  front: `#${v.hex}`,
-  back: "#f6f6f6",
-}));
--- src/models/HTMLElementEvent.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @sees https://qiita.com/wamei/items/43753e03821964719f31
- */
-export interface HTMLElementEvent<T extends HTMLElement> extends Event {
-  target: T;
-}
